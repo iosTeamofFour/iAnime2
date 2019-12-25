2fd5695d6188f7eb44fad711f46861b66b904c67
WindyZYY <2461607010@qq.com>
2019-12-25 22:34:08 +0800

    hhh

--- app/src/main/java/com/nemesiss/dev/ianime/Acitivity/WorkDetailActivity.java
+++ app/src/main/java/com/nemesiss/dev/ianime/Acitivity/WorkDetailActivity.java
@@ -1,6 +1,8 @@
 package com.nemesiss.dev.ianime.Acitivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.MenuItem;
 import com.nemesiss.dev.ianime.R;
 
 public class WorkDetailActivity extends iAnimeActivity {
@@ -10,4 +12,14 @@ public class WorkDetailActivity extends iAnimeActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_detail);
     }
+//    @Override
+//    protected void onStart(){
+//        super.onStart();
+//        ActionBar actionBar = this.getActionBar();
+//        actionBar.setDisplayHomeAsUpEnabled(true);
+//    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item){
+//        return super.onOptionsItemSelected(item);
+//    }
 }
