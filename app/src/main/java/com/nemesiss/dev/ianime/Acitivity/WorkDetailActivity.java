abbfa75b54d959a96b9863443dc0cc8d03adb16c
WindyZYY <2461607010@qq.com>
2019-12-26 12:08:57 +0800

    不知道说啥

--- app/src/main/java/com/nemesiss/dev/ianime/Acitivity/WorkDetailActivity.java
+++ app/src/main/java/com/nemesiss/dev/ianime/Acitivity/WorkDetailActivity.java
@@ -1,6 +1,9 @@
 package com.nemesiss.dev.ianime.Acitivity;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -8,7 +11,10 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 import com.nemesiss.dev.ianime.R;
+import com.nemesiss.dev.ianime.View.PinchImageView;
 
 
 public class WorkDetailActivity extends iAnimeActivity {
@@ -17,6 +23,31 @@ public class WorkDetailActivity extends iAnimeActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_detail);
+
+        ImageView showBigPic = (ImageView) findViewById(R.id.works_img);
+        showBigPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WorkDetailActivity.this, PinchImageView.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView openMenu = (ImageView) findViewById(R.id.menu);
+        openMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemsListDialog();
+            }
+        });
+
+        ImageView likeWorks = (ImageView) findViewById(R.id.ifLike);
+        likeWorks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
     protected void setImage(Image img){
 
@@ -38,5 +69,35 @@ public class WorkDetailActivity extends iAnimeActivity {
     }
     protected void setLike(boolean like){
 
+    }
+    private void itemsListDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(WorkDetailActivity.this);
+        final AlertDialog dialog = builder.create();
+        View dialogView = View.inflate(WorkDetailActivity.this,R.layout.dialog_item,null);
+        dialog.setView(dialogView);
+        dialog.show();
+
+        final TextView checkDraft = dialogView.findViewById(R.id.checkDraft);
+        final TextView saveSplash = dialogView.findViewById(R.id.saveSplash);
+        final TextView saveDraft = dialogView.findViewById(R.id.saveDraft);
+
+        checkDraft.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });
+        saveSplash.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });
+        saveDraft.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });
     }
 }
